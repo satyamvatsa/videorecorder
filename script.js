fa3bad5b8d6b16a45f97fcb2f01c6dc96e18afc9
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2022-03-19 01:06:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+let mediaRecorder;
+let recordedBlobs;
+
 let startcam = document.querySelector('#startcam');
 let camicon = document.querySelector('#camicon');
 let recordbtn = document.querySelector('#recordbtn');
@@ -9,11 +12,24 @@ let heading = document.querySelector('.heading');
 let recordbutton = document.querySelector('#recordbutton');
 let secondscreen = document.querySelector('.secondscreen');
 let retake = document.querySelector('.retakebtn');
+let recwindow = document.querySelector('#recwindow');
 
 startcam.addEventListener('click',() => {
+    if(navigator.mediaDevices.getUserMedia){
+        recordButton.disabled = false;
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+
+  recwindow.srcObject = stream;
+    }
+    else{
+        console.log("getUserMedia not supported!");
+    }
     camicon.innerHTML="videocam";
     startcam.style.backgroundColor="transparent";
 });
+
+
 
 retake.addEventListener('click',() => {
     window.location.reload()
@@ -31,3 +47,16 @@ recordbtn.addEventListener('click',() => {
         secondscreen.style.display="block";
     }
 });
+
+if(navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video:true})
+    .then(function(stream){
+        recwindow.srcObject = stream;
+    })
+    .catch (function(error){
+        console.log("Somthing went wrong");
+    })
+}
+else{
+    console.log("getUserMedia not supported!");
+}
