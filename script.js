c6e1c9f625486eb3368fa86764b7dc43827f0e67
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2022-03-19 20:25:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,137 +1,238 @@
 'use strict';
 
+/* globals MediaRecorder */
+
 let mediaRecorder;
 let recordedBlobs;
 
-let startcam = document.querySelector('#startcam');
-let camicon = document.querySelector('#camicon');
-let recordbtn = document.querySelector('#recordbtn');
-let uploadpre = document.querySelector('.uploadpre');
+const errorMsgElement = document.querySelector('span#errorMsg');
+const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+const playButton = document.querySelector('button#play');
+const downloadButton = document.querySelector('button#download');
 let videopre = document.querySelector('.videopre');
-let heading = document.querySelector('.heading');
-let recordbutton = document.querySelector('#recordbutton');
 let secondscreen = document.querySelector('.secondscreen');
+let recwindow = document.querySelector('.recwindow');
+let recordbtn = document.querySelector('.recordbtn');
+let uploadpre = document.querySelector('.uploadpre');
+let heading = document.querySelector('.heading');
+let prewindow = document.querySelector('.prewindow');
+let videobtn = document.querySelector('.videobtn');
 let retake = document.querySelector('.retakebtn');
-let recwindow = document.querySelector('#recwindow');
-let playbtn = document.querySelector('#playbtn');
-let prewindow = document.querySelector('#prewindow');
-let uploadbtn = document.querySelector('.uploadbtn');
-
-startcam.addEventListener('click',() => {
-    camicon.innerHTML="videocam";
-    startcam.style.backgroundColor="transparent";
-});
-
-
 
 retake.addEventListener('click',() => {
     window.location.reload()
 });
 
-recordbtn.addEventListener('click',() => {
-    if (recordbutton.textContent === 'Start Recording') {
-        startRecording();
-        uploadpre.style.display="none";
-        videopre.classList.remove('col-lg-7');
-        heading.style.display="none";
-        recordbutton.textContent = 'Stop Recording';
-    } else {
-        stopRecording();
-        recordbtn.style.display="none";
-        secondscreen.style.display="block";
-        prewindow.style.display="block";
-        recwindow.style.display="none";
-    }
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Record') {
+    startRecording();
+    videopre.classList.remove('col-lg-7');
+    heading.style.display="none";
+    uploadpre.style.display="none";
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Record';
+    playButton.disabled = false;
+    downloadButton.disabled = false;
+    retake.disabled=false;
+    secondscreen.style.display="block";
+    recwindow.style.display="none";
+    recordbtn.style.display="none";
+    prewindow.style.display="block";
+    videobtn.style.display="none";
+  }
 });
 
-playbtn.addEventListener('click', () => {
-    const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-    prewindow.src = null;
-    prewindow.srcObject = null;
-    prewindow.src = window.URL.createObjectURL(superBuffer);
-    prewindow.controls = true;
-    prewindow.play();
+
+playButton.addEventListener('click', () => {
+  const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+  recordedVideo.src = null;
+  recordedVideo.srcObject = null;
+  recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  recordedVideo.controls = true;
+  recordedVideo.play();
 });
 
-uploadbtn.addEventListener('click', () => {
-    const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'test.mp4';
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(() => {
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    }, 100);
-  });
+
+downloadButton.addEventListener('click', () => {
+  const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.mp4';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+});
 
 function handleDataAvailable(event) {
-    console.log('handleDataAvailable', event);
-    if (event.data && event.data.size > 0) {
-        recordedBlobs.push(event.data);
-    }
+  console.log('handleDataAvailable', event);
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
+  }
 }
 
 function startRecording() {
-    recordedBlobs = [];
-    let options = {mimeType: 'video/webm;codecs=vp9,opus'};
-    try {
-      mediaRecorder = new MediaRecorder(window.stream, options);
-    } catch (e) {
-      console.error('Exception while creating MediaRecorder:', e);
-      errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-      return;
-    }
+  recordedBlobs = [];
+  let options = {mimeType: 'video/webm;codecs=vp9,opus'};
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
 
-    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-    recordbutton.textContent = 'Stop Recording';
-    playbtn.disabled = true;
-    uploadbtn.disabled = true;
-    mediaRecorder.onstop = (event) => {
-      console.log('Recorder stopped: ', event);
-      console.log('Recorded Blobs: ', recordedBlobs);
-    };
-    mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.start();
-    console.log('MediaRecorder started', mediaRecorder);
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
 }
 
 function stopRecording() {
-    mediaRecorder.stop();
+  mediaRecorder.stop();
 }
 
 function handleSuccess(stream) {
-    recordbutton.disabled = false;
-    console.log('getUserMedia() got stream:', stream);
-    window.stream = stream;
-  
-    const gumVideo = document.querySelector('video#recwindow');
-    gumVideo.srcObject = stream;
+  recordButton.disabled = false;
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+
+  const gumVideo = document.querySelector('video#gum');
+  gumVideo.srcObject = stream;
 }
 
 async function init(constraints) {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      handleSuccess(stream);
-    } catch (e) {
-      console.error('navigator.getUserMedia error:', e);
-      errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
-    }
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
   }
+}
 
-  document.querySelector('button#startcam').addEventListener('click', async () => {
-    const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
-    const constraints = {
-      audio: {
-        echoCancellation: {exact: hasEchoCancellation}
-      },
-      video: {
-        width: 1280, height: 720
-      }
-    };
-    console.log('Using media constraints:', constraints);
-    await init(constraints);
+document.querySelector('button#start').addEventListener('click', async () => {
+  const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
+  const constraints = {
+    audio: {
+      echoCancellation: {exact: hasEchoCancellation}
+    },
+    video: {
+      width: 1280, height: 720
+    }
+  };
+  console.log('Using media constraints:', constraints);
+  await init(constraints);
+});
+
+
+
+// File upload start
+
+
+const form = document.querySelector("form"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+let urlcopy = document.querySelector('.showurl');
+
+// form click event
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+  urlcopy.style.display="block";
+  let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
+  if(file){
+    let fileName = file.name; //getting file name
+    if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName); //calling uploadFile with passing file name as an argument
+  }
+}
+
+// file upload function
+function uploadFile(name){
+  let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
+  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
+  xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
+    let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
+    let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
+    let fileSize;
+    // if file size is less than 1024 then add only KB else convert this KB into MB
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
+                          <i class="fas fa-file-alt"></i>
+                          <div class="content">
+                            <div class="details">
+                              <span class="name">${name} • Uploading</span>
+                              <span class="percent">${fileLoaded}%</span>
+                            </div>
+                            <div class="progress-bar">
+                              <div class="progress" style="width: ${fileLoaded}%"></div>
+                            </div>
+                          </div>
+                        </li>`;
+    // uploadedArea.innerHTML = ""; //uncomment this line if you don't want to show upload history
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if(loaded == total){
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
+                            <div class="content upload">
+                              <i class="fas fa-file-alt"></i>
+                              <div class="details">
+                                <span class="name">${name} • Uploaded</span>
+                                <span class="size">${fileSize}</span>
+                              </div>
+                            </div>
+                            <i class="fas fa-check"></i>
+                          </li>`;
+      uploadedArea.classList.remove("onprogress");
+      // uploadedArea.innerHTML = uploadedHTML; //uncomment this line if you don't want to show upload history
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //remove this line if you don't want to show upload history
+    }
   });
+  let data = new FormData(form); //FormData is an object to easily send form data
+  xhr.send(data); //sending form data
+}
+
+
+
+//Copy link start
+let copytext = document.querySelector('#copytext');
+
+copytext.addEventListener('click',() => {
+  myFunction();
+});
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
