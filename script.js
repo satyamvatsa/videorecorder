83c545c8d223edd2179406f6164417928aed4af0
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2022-03-24 00:03:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -282,3 +282,7 @@ function uploadFile(name){
   let data = new FormData(form); //FormData is an object to easily send form data
   xhr.send(data); //sending form data
 }
+
+$(document).ready(function(){
+  $("#myModal").modal('show');
+});
