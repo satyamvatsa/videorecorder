3e4217cff18c5363961d46fcc71a3e3c77745d60
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2022-03-23 17:30:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,47 +10,56 @@ const recordedVideo = document.querySelector('video#recorded');
 const recordButton = document.querySelector('button#record');
 const playButton = document.querySelector('button#play');
 const downloadButton = document.querySelector('button#download');
-let videopre = document.querySelector('.videopre');
-let secondscreen = document.querySelector('.secondscreen');
-let recwindow = document.querySelector('.recwindow');
-let recordbtn = document.querySelector('.recordbtn');
-let uploadpre = document.querySelector('.uploadpre');
-let heading = document.querySelector('.heading');
-let prewindow = document.querySelector('.prewindow');
-let videobtn = document.querySelector('.videobtn');
+const uploadsection = document.querySelector('.uploadsection');
+const mainrecord = document.querySelector('#mainrecord');
+const blockquote = document.querySelector('.blockquote');
+const heading = document.querySelector('.heading');
+const secondscreen = document.querySelector('.secondscreen');
+const recorded = document.querySelector('#recorded');
+const gum = document.querySelector('.gum');
 let retake = document.querySelector('.retakebtn');
 let startvid = document.querySelector('.startvid');
 let icon = document.querySelector('.startvid span');
-let recordedlink = document.querySelector('.videopre .recordedlink');
-let processing = document.querySelector('#processing');
+let progressbar = document.querySelector('.progressbar');
+let copyandprocess = document.querySelector('.copyandprocess');
+let processedvideobtn = document.querySelector('.processedvideobtn');
+
+downloadButton.addEventListener('click',() => {
+  secondscreen.style.display="none";
+  recorded.style.display="none";
+  progressbar.style.display="block";
+  copyandprocess.style.display="block";
+  processedvideobtn.style.display="block";
+});
 
 startvid.addEventListener('click',() => {
   icon.textContent="videocam";
   startvid.style.backgroundColor="transparent";
 });
 
-
 retake.addEventListener('click',() => {
-    window.location.reload()
+  window.location.reload()
 });
 
 recordButton.addEventListener('click', () => {
   if (recordButton.textContent === 'Record') {
     startRecording();
-    videopre.classList.remove('col-lg-7');
+    uploadsection.style.display="none";
+    mainrecord.classList.remove('col-lg-7');
+    mainrecord.classList.add('col-lg-12');
+    blockquote.style.display="none";
     heading.style.display="none";
-    uploadpre.style.display="none";
+    
   } else {
     stopRecording();
     recordButton.textContent = 'Record';
     playButton.disabled = false;
     downloadButton.disabled = false;
-    retake.disabled=false;
     secondscreen.style.display="block";
-    recwindow.style.display="none";
-    recordbtn.style.display="none";
-    prewindow.style.display="block";
-    videobtn.style.display="none";
+    recorded.style.display="block";
+    gum.style.display="none";
+    recordButton.style.display="none";
+    startvid.style.display="none";
   }
 });
 
@@ -69,10 +78,6 @@ downloadButton.addEventListener('click', () => {
   const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
   const url = window.URL.createObjectURL(blob);
   const a = document.createElement('a');
-  recordedlink.style.display="block";
-  processing.style.display="block";
-  secondscreen.style.display="none";
-  prewindow.style.display="none";
   a.style.display = 'none';
   a.href = url;
   a.download = 'test.mp4';
@@ -82,6 +87,9 @@ downloadButton.addEventListener('click', () => {
     document.body.removeChild(a);
     window.URL.revokeObjectURL(url);
   }, 100);
+  stream.getTracks().forEach(function(track){
+    track.stop();
+    })
 });
 
 function handleDataAvailable(event) {
@@ -151,107 +159,6 @@ document.querySelector('button#start').addEventListener('click', async () => {
   console.log('Using media constraints:', constraints);
   await init(constraints);
 });
-
-
-
-// File upload start
-
-
-const form = document.querySelector("#uploadform"),
-fileInput = document.querySelector(".file-input"),
-progressArea = document.querySelector(".progress-area"),
-uploadedArea = document.querySelector(".uploaded-area");
-let urlcopy = document.querySelector('.showurl');
-
-// form click event
-form.addEventListener("click", () =>{
-  fileInput.click();
-});
-
-fileInput.onchange = ({target})=>{
-  urlcopy.style.display="block";
-  let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
-  if(file){
-    let fileName = file.name; //getting file name
-    if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
-      let splitName = fileName.split('.');
-      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-    }
-    uploadFile(fileName); //calling uploadFile with passing file name as an argument
-  }
-}
-
-// file upload function
-function uploadFile(name){
-  let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
-  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
-  xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
-    let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
-    let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
-    let fileSize;
-    // if file size is less than 1024 then add only KB else convert this KB into MB
-    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-    let progressHTML = `<li class="row">
-                          <i class="fas fa-file-alt"></i>
-                          <div class="content">
-                            <div class="details">
-                              <span class="name">${name} • Uploading</span>
-                              <span class="percent">${fileLoaded}%</span>
-                            </div>
-                            <div class="progress-bar">
-                              <div class="progress" style="width: ${fileLoaded}%"></div>
-                            </div>
-                          </div>
-                        </li>`;
-    // uploadedArea.innerHTML = ""; //uncomment this line if you don't want to show upload history
-    uploadedArea.classList.add("onprogress");
-    progressArea.innerHTML = progressHTML;
-    if(loaded == total){
-      progressArea.innerHTML = "";
-      let uploadedHTML = `<li class="row">
-                            <div class="content upload">
-                              <i class="fas fa-file-alt"></i>
-                              <div class="details">
-                                <span class="name">${name} • Uploaded</span>
-                                <span class="size">${fileSize}</span>
-                              </div>
-                            </div>
-                            <i class="fas fa-check"></i>
-                          </li>`;
-      uploadedArea.classList.remove("onprogress");
-      // uploadedArea.innerHTML = uploadedHTML; //uncomment this line if you don't want to show upload history
-      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //remove this line if you don't want to show upload history
-    }
-  });
-  let data = new FormData(form); //FormData is an object to easily send form data
-  xhr.send(data); //sending form data
-}
-
-
-
-//Copy link start
-let copytext = document.querySelector('#copytext');
-
-copytext.addEventListener('click',() => {
-  myFunction();
-});
-
-function myFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
-  
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
-
-
 
 /* Mobile video */
 
