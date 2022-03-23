0b7cf646f424b26ef651a6864c3c67648ed6fd55
satyam vatsa <66280108+satyamvatsa@users.noreply.github.com>
2022-03-23 23:33:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,6 +23,20 @@ let icon = document.querySelector('.startvid span');
 let progressbar = document.querySelector('.progressbar');
 let copyandprocess = document.querySelector('.copyandprocess');
 let processedvideobtn = document.querySelector('.processedvideobtn');
+let mobileupload = document.querySelector('.mobileupload');
+let mobilebtn = document.querySelector('.mobilebtn');
+let mobilevideo = document.querySelector('.mobilevideo');
+let mobilerecoder = document.querySelector('.mobilerecoder');
+let recordandupload = document.querySelector('.recordandupload');
+
+mobileupload.addEventListener('click',() => {
+  mobilebtn.style.display="none";
+  mobilevideo.style.display="none";
+  progressbar.style.display="block";
+  copyandprocess.style.display="block";
+  processedvideobtn.style.display="block";
+  mobilerecoder.style.height="60vh";
+});
 
 downloadButton.addEventListener('click',() => {
   secondscreen.style.display="none";
@@ -175,14 +189,96 @@ document.addEventListener('DOMContentLoaded', (ev)=>{
       }
       else if(input.files[0].type.indexOf("video/") > -1 ){
           let video = document.getElementById('video');
-          let videowin = document.querySelector('.videowin');
-          let showmobileupload = document.querySelector('.showmobileupload');
+          
+          let mainmobilerecorder = document.querySelector('.mainmobilerecorder');
+          let fileupload = document.querySelector('.fileupload');
           video.src=window.URL.createObjectURL(input.files[0]);
-          videowin.style.display="block";
-          form.style.display="none";
-          uploadpre.style.display="none";
-          showmobileupload.style.display="block";
+          mobilebtn.style.display="block";
+          mobilevideo.style.display="block";
+          mainmobilerecorder.style.display="none";
+          fileupload.style.display="none";
+          heading.style.display="none";
       }
   })
   
 })
+
+
+const form = document.querySelector("#uploadform"),
+fileInput = document.querySelector(".file-input"),
+progressArea = document.querySelector(".progress-area"),
+uploadedArea = document.querySelector(".uploaded-area");
+let urlcopy = document.querySelector('.showurl');
+
+// form click event
+form.addEventListener("click", () =>{
+  fileInput.click();
+});
+
+fileInput.onchange = ({target})=>{
+  urlcopy.style.display="block";
+  progressbar.style.display="block";
+  copyandprocess.style.display="block";
+  processedvideobtn.style.display="block";
+  recordandupload.style.display="none";
+  heading.style.display="none";
+  recorded.style.display="none";
+    gum.style.display="none";
+    recordButton.style.display="none";
+    startvid.style.display="none";
+  let file = target.files[0]; //getting file [0] this means if user has selected multiple files then get first one only
+  if(file){
+    let fileName = file.name; //getting file name
+    if(fileName.length >= 12){ //if file name length is greater than 12 then split it and add ...
+      let splitName = fileName.split('.');
+      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
+    }
+    uploadFile(fileName); //calling uploadFile with passing file name as an argument
+  }
+}
+
+// file upload function
+function uploadFile(name){
+  let xhr = new XMLHttpRequest(); //creating new xhr object (AJAX)
+  xhr.open("POST", "php/upload.php"); //sending post request to the specified URL
+  xhr.upload.addEventListener("progress", ({loaded, total}) =>{ //file uploading progress event
+    let fileLoaded = Math.floor((loaded / total) * 100);  //getting percentage of loaded file size
+    let fileTotal = Math.floor(total / 1000); //gettting total file size in KB from bytes
+    let fileSize;
+    // if file size is less than 1024 then add only KB else convert this KB into MB
+    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
+    let progressHTML = `<li class="row">
+                          <i class="fas fa-file-alt"></i>
+                          <div class="content">
+                            <div class="details">
+                              <span class="name">${name} • Uploading</span>
+                              <span class="percent">${fileLoaded}%</span>
+                            </div>
+                            <div class="progress-bar">
+                              <div class="progress" style="width: ${fileLoaded}%"></div>
+                            </div>
+                          </div>
+                        </li>`;
+    // uploadedArea.innerHTML = ""; //uncomment this line if you don't want to show upload history
+    uploadedArea.classList.add("onprogress");
+    progressArea.innerHTML = progressHTML;
+    if(loaded == total){
+      progressArea.innerHTML = "";
+      let uploadedHTML = `<li class="row">
+                            <div class="content upload">
+                              <i class="fas fa-file-alt"></i>
+                              <div class="details">
+                                <span class="name">${name} • Uploaded</span>
+                                <span class="size">${fileSize}</span>
+                              </div>
+                            </div>
+                            <i class="fas fa-check"></i>
+                          </li>`;
+      uploadedArea.classList.remove("onprogress");
+      // uploadedArea.innerHTML = uploadedHTML; //uncomment this line if you don't want to show upload history
+      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML); //remove this line if you don't want to show upload history
+    }
+  });
+  let data = new FormData(form); //FormData is an object to easily send form data
+  xhr.send(data); //sending form data
+}
